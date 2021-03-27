775223468a6ad965173bb0bf65915a5fbb694b6e
Vinicius Santana <vinicius.santana@localiza.com>
2021-03-27 20:32:52 -0300

    Change to acess Course API for show in a classroom

--- src/app/modules/calculate-dividers/calculate-dividers.component.ts
+++ src/app/modules/calculate-dividers/calculate-dividers.component.ts
@@ -11,9 +11,13 @@ export class CalculateDividersComponent implements OnInit {
   loading: boolean = false;
   forms: FormGroup;
   numbers: any;
+
+  TypeEnum: any = [{id:1, description: "TI"}, {id: 2, description: "Cooking"}]
+
   constructor(private formBuilder: FormBuilder, private calculateDividersService: CalculateDividersService) {
     this.forms = this.formBuilder.group({
-      Number: ['', [Validators.required, Validators.min(1)]]
+      Name: ['', [Validators.required]],
+      Type: ['']
     });
 
   }
@@ -23,18 +27,38 @@ export class CalculateDividersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getById();
+  }
+
+  GetAll(){
+    this.calculateDividersService.GetAll().subscribe((response)=>{
+      console.log(response, " GetAll ");
+    })
+  }
+
+  getById(){
+    this.calculateDividersService.GetById().subscribe((response)=>{
+      console.log(response, " GetById ");
+    })
   }
 
   calculate(obj){
-    this.loading = true;
-    obj.Number = Number(obj.Number);
-    this.numbers = {};
-    this.calculateDividersService.calculate(obj).subscribe((res: any) => {
-      this.numbers = res.result;
-      setTimeout(() => {
-        this.loading = false;
-      }, 500);
-    })
+    obj.Type = parseInt(obj.Type);
+    console.log(obj);
+
+    this.calculateDividersService.Register(obj).subscribe((response) => {
+      console.log(response);
+    });
+
+    // this.loading = true;
+    // obj.Number = Number(obj.Number);
+    // this.numbers = {};
+    // this.calculateDividersService.calculate(obj).subscribe((res: any) => {
+    //   this.numbers = res.result;
+    //   setTimeout(() => {
+    //     this.loading = false;
+    //   }, 500);
+    // })
   }
 
 }
--- src/app/modules/calculate-dividers/calculate-dividers.service.ts
+++ src/app/modules/calculate-dividers/calculate-dividers.service.ts
@@ -5,11 +5,25 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class CalculateDividersService {
-  private URL: string = 'https://localhost:44393/api/Divider'
+  private URL: string = 'https://localhost:44335/api/Course/';
   constructor(private http: HttpClient) { }
 
   calculate(obj){
     console.log(obj);
     return this.http.post(this.URL, obj);
   }
+
+  Register(course){
+    return this.http.post(this.URL, course);
+  }
+
+  GetAll(){
+    return this.http.get(this.URL);
+  }
+
+  GetById(){
+    var id = "363ba979-578b-418f-2d66-08d8ed80bb6b";
+    return this.http.get(this.URL + "GetById?courseId=" + id);
+  }
+
 }
